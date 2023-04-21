b0afa097b5994332f6155b3fd53d274e97590d5e
0xGodspeed <74945202+0xGodspeed@users.noreply.github.com>
2023-04-21 14:43:52 +0530

    added instructionToBinary function

--- assembler.py
+++ assembler.py
@@ -1,4 +1,5 @@
 from sys import argv
+from constants import opcode
 
 input_file = argv[1]
 instructions = []
@@ -9,6 +10,17 @@ with open(input_file) as file:
 # clean whitespaces and split each instruction
 # [['var', 'X'], ['mov', 'R1', '$10']]
 instructions = [i.strip().split() for i in instructions]
+
+
+def instructionToBinary(instruction: list[int]) ->  str:
+    # add reg1 reg2 reg3
+    binary_instruction = ''    
+    binary_instruction += opcode[instruction][0]
+
+    if opcode[instruction][1] == "A":
+        pass
+    elif opcode[instruction][1] == "B":
+        pass
 
 
 
