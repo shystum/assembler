1f8951b3ade7365339190af9e8a2201752b2aba1
Dikshant Agarwal <117189990+darknight2135@users.noreply.github.com>
2023-06-01 05:14:18 +0530

    added integertosixteenbit function

--- SimpleSimulator/convertors.py
+++ SimpleSimulator/convertors.py
@@ -3,3 +3,6 @@ def integerToSevenBitBinary(n: int) -> str:
 
 def sevenBitBinaryToInteger(n: str) -> int:
     return int(n, 2)
+
+def integerToSixteenBitBinary(n: int) -> str:
+    return bin(n)[2:].zfill(16)
