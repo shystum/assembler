2ac635122d13c35a078ba4aca2344820214a0181
13100D <78998762+13100D@users.noreply.github.com>
2023-06-02 20:27:51 +0530

    enable implemented dumps

--- SimpleSimulator/simulator.py
+++ SimpleSimulator/simulator.py
@@ -6,8 +6,7 @@ halted = False
 
 while not halted:
     Instruction = mem.getInstruction(mem.pc_counter)
-    halted,mem.pc_counter = ee.execute(Instruction,mem, rf)
-    #pc.dump()
-    #pc.dump()
-    # rf.dump()
+    mem.pc_dump()
+    halted, mem.pc_counter = ee.execute(Instruction,mem, rf)
+    rf.dump()
     #pc.update(new_pc)
